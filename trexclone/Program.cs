5e44a370e6a564994bddbe8a319e14df7f853ec8
Laffyyy <95970405+Laffyyy@users.noreply.github.com>
2024-04-18 20:45:53 +0800

    Initial commit

---
+++ trexclone/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T_Rex_Endless_Runner_MOO_ICT
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// idk what the hell this is
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
